4eb5aed9d9379cf1d06ed52956d903fafbb33a7c
Aleksandr Kataev <akataev1819@gmail.com>
2020-07-28 16:01:12 +0100

    Fixed jest error, cleaned up react-create-app

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const path = require('path');
 const dbHandler = require('./db_handler');
+
+const publicPath = path.join(__dirname, './client/build');
 
 const app = express();
 
@@ -15,5 +18,13 @@ if (process.env.NODE_ENV === 'test') {
 // Routes
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/user', require('./routes/api/user'));
+
+// Serve assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(publicPath));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+  });
+}
 
 module.exports = app;
